6c8f9a208b9620b42db0a9ad9f024960192029e8
Ubaid Ullah <ubaidwaqar71@gmail.com>
2024-12-24 19:43:46 +0500

    Updated app with new changes

--- src/components/contact-header/index.jsx
+++ src/components/contact-header/index.jsx
@@ -20,7 +20,7 @@ const ContactHeader = () => {
         <div className="header-btn1">
           {/* Resume download button */}
           <a 
-            href="public\Mubeen Iqbal Full Stack Developer-.pdf" 
+            href="/images/pdf/Mubeen Iqbal Full Stack Developer-.pdf" 
             className="button-link1" 
             download="Mubeen_Iqbal_Resume.pdf"
           >
