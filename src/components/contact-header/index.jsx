dbc986666eeb4a521694fbdcedcb907c99bf33e9
Ubaid Ullah <ubaidwaqar71@gmail.com>
2024-12-27 19:56:21 +0500

    first commit

--- src/components/contact-header/index.jsx
+++ src/components/contact-header/index.jsx
@@ -21,7 +21,7 @@ const ContactHeader = () => {
         <div className="header-btn1">
           {/* Resume download button */}
           <a 
-            href="image/mubeen-iqbal-full-stack-dev-.pdf" 
+            href="image/mubeen-iqbal-full-stack-dev.pdf" 
             className="button-link1" 
             download="Mubeen_Iqbal_Resume.pdf"
           >
